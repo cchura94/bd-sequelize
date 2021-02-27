c3470d7e4b79b19f4e9771ee0027373f87f8e4b2
cchura94 <cchura.cpc@gmail.com>
2021-02-27 10:45:32 -0400

    Sequelize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 //importacion
 const express = require("express");
 const bd = require("./bd");
+
 const usuario = require("./models/index").Usuario
 const Role = require("./models/index").Role
+const Persona = require("./models/index").Persona
+const Categoria = require("./models/index").Categoria
+const Producto = require("./models/index").Producto
+const RoleUsuario = require("./models/index").RoleUsuario
 // migraciones
 usuario.sync({ force: true });
 Role.sync({ force: true });
+Persona.sync({ force: true });
+Categoria.sync({ force: true });
+Producto.sync({ force: true });
+RoleUsuario.sync({ force: true });
 // 1 : 1
 // 1 : N
 // N : M
--- src/models/index.js
+++ src/models/index.js
@@ -1,24 +1,77 @@
 const Sequelize = require("sequelize")
 const bd = require("./../bd");
 
-const Usuario = bd.define("usuario", {
+
+const Usuario = bd.define("Usuario", {
     correo: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(50),
         unique:  true
     },
-    clave: Sequelize.STRING,
-    estado: Sequelize.BOOLEAN
+    clave: Sequelize.STRING(30),
+    estado: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
 });
 
-const Role = bd.define("rol", {
+// Persona
+const Persona = bd.define("Persona", {
+    nombres: Sequelize.STRING,
+    apellidos: Sequelize.STRING,
+    ci: Sequelize.STRING(15),
+    usuario_id: {
+        type: Sequelize.INTEGER,
+        references: {model: Usuario, key: 'id'}
+    }
+},{
+
+})
+// Relacion de Uno a Uno
+Usuario.hasOne(Persona, {foreignKey: 'usuario_id'});
+Persona.belongsTo(Usuario, {foreignKey: 'usuario_id'});
+
+const Categoria = bd.define("Categoria", {
     nombre: {
-        type: Sequelize.STRING,
-        
+        type: Sequelize.STRING(50),
+        unique: true
     },
     descripcion: Sequelize.TEXT
 });
 
+const Producto = bd.define("Producto", {
+    nombre: Sequelize.STRING,
+    cantidad: Sequelize.INTEGER,
+    precio: Sequelize.DECIMAL(10,2),
+    categoria_id: {
+        type: Sequelize.INTEGER,
+        references: {model: Categoria, key: 'id'}
+    } 
+});
+
+// Relación UNO a MUCHOS
+Categoria.hasMany(Producto, {foreignKey: 'categoria_id'});
+//Producto.belongsTo(Categoria, {foreignKey: 'categoria_id'});
+
+
+const Role = bd.define("Role", {
+    nombre: {
+        type: Sequelize.STRING(30),        
+    },
+    descripcion: Sequelize.TEXT
+});
+
+//Relacion de N a M
+const RoleUsuario = bd.define('role_usuario'); 
+Usuario.belongsToMany(Role, {through: RoleUsuario});
+Role.belongsToMany(Usuario, {through: RoleUsuario});
+
 module.exports = {
     Usuario,
-    Role
+    Role,
+    Persona,
+    Categoria,
+    Producto,
+    RoleUsuario
+
 }
